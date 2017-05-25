19779ff85114c4bdd3cf62d83d420f20c957a434
Bojidar Sokolov <bebosvg@abv.bg>
2017-05-25 21:43:45 +0300

    Update extra.php

--- extra.php
+++ extra.php
@@ -1,8 +1,8 @@
 <?php
-	$eventDate = get_post_meta( get_the_ID(), 'Event Date', true );
-	if( empty( $eventDate) ) : echo "Empty Record"; endif; 
-	$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
-	if( empty( $eventLocation) ) : echo "Empty Record"; endif;
+$eventDate = get_post_meta( get_the_ID(), 'Event Date', true );
+if( empty( $eventDate) ) : echo "Empty Record"; endif; 
+$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
+if( empty( $eventLocation) ) : echo "Empty Record"; endif;
 $html = '<div class = "event-meta">
 	<strong>Event date: </strong> '.$eventDate.'<br>
 	<strong>Event Location: </strong> '.$eventLocation.'<br>
