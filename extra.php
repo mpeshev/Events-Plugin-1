9f687bc84846f84cfabcaf08663301d655a99485
Bojidar Sokolov <bebosvg@abv.bg>
2017-05-25 21:04:05 +0300

    Update extra.php

--- extra.php
+++ extra.php
@@ -1,7 +1,8 @@
 <?php
 	$eventDate = get_post_meta( get_the_ID(), 'Event Date', true );
+	if( empty( $eventDate) ) : echo "Empty Record"; endif; 
 	$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
-	$url = get_post_meta( get_the_ID(), 'Link to the book:' , true);
+	if( empty( $eventLocation) ) : echo "Empty Record"; endif;
 $html = '<div class = "event-meta">
 	<strong>Event date: </strong> '.$eventDate.'<br>
 	<strong>Event Location: </strong> '.$eventLocation.'<br>
