33e6cb99e6c159f2557c48a1da5a12c02219d603
Bojidar Sokolov <bebosvg@abv.bg>
2017-05-25 16:32:08 +0300

    Add files via upload

---
+++ extra.php
@@ -0,0 +1,8 @@
+<?php
+	$eventDate = get_post_meta( get_the_ID(), 'Event Date', true );
+	$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
+	$url = get_post_meta( get_the_ID(), 'Link to the book:' , true);
+$html = '<div class = "event-meta">
+			<strong>Event date: </strong> '.$eventDate.'<br>
+			<strong>Event Location: </strong> '.$eventLocation.'<br>
+			</div>';
