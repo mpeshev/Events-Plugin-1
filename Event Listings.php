d3fb9f3aa4b78d54831979101e7f5f910ce302d3
Bojidar Sokolov <bebosvg@abv.bg>
2017-05-25 21:18:32 +0300

    Update Event Listings.php

--- Event Listings.php
+++ Event Listings.php
@@ -11,6 +11,9 @@
 /**
 * Creating new class for Events.
 */
+if (class_exists('EventListings')) {
+    $EventListings = new EventListings();
+}
 class EventListings
 {
 	function __construct()
@@ -21,53 +24,47 @@ class EventListings
 	public function event_listing() {
 		register_post_type( 'eventing', array(
 				'labels' 					=> array(
-				'name' 					=> __("Events ", 'eventbase'),
-				'singular_name'			=> __("Event", 'eventbase'),
-				'add_new' 				=> _x("Add New", 'eventing', 'eventbase' ),
-				'add_new_item' 			=> __("Add New Event", 'eventbase' ),
-				'edit_item' 			=> __("Edit Event", 'eventbase' ),
-				'new_item' 				=> __("New Event", 'eventbase' ),
-				'view_item' 			=> __("View Event", 'eventbase' ),
-				'search_items' 			=> __("Search Events ", 'eventbase' ),
-				'not_found' 			=>  __("No Events  found", 'eventbase' ),
-				'not_found_in_trash' 	=> __("No Events  found in Trash", 'eventbase' ),
+				'name' 						=> __("Events ", 'eventbase'),
+				'singular_name'					=> __("Event", 'eventbase'),
+				'add_new' 					=> _x("Add New", 'eventing', 'eventbase' ),
+				'add_new_item' 					=> __("Add New Event", 'eventbase' ),
+				'edit_item' 					=> __("Edit Event", 'eventbase' ),
+				'new_item' 					=> __("New Event", 'eventbase' ),
+				'view_item' 					=> __("View Event", 'eventbase' ),
+				'search_items' 					=> __("Search Events ", 'eventbase' ),
+				'not_found' 					=>  __("No Events  found", 'eventbase' ),
+				'not_found_in_trash' 				=> __("No Events  found in Trash", 'eventbase' ),
 			),
-				'description' 			=> __("Events  for the demo", 'eventbase'),
-				'public' 				=> true,
-				'publicly_queryable' 	=> true,
-				'query_var' 			=> true,
-				'rewrite' 				=> true,
-				'has_archive'			=> true,
-				'exclude_from_search' 	=> true,
-				'show_ui' 				=> true,
-				'show_in_menu'			=> true,
-				'menu_position' 		=> 11, // may cause problems
-				'supports' 				=> array(
+				'description' 					=> __("Events  for the demo", 'eventbase'),
+				'public' 					=> true,
+				'publicly_queryable' 				=> true,
+				'query_var' 					=> true,
+				'rewrite' 					=> true,
+				'has_archive'					=> true,
+				'exclude_from_search' 				=> true,
+				'show_ui' 					=> true,
+				'show_in_menu'					=> true,
+				'menu_position' 				=> 11, // may cause problems
+				'supports' 					=> array(
 				'title',
 				'editor',
 				'thumbnail',
 				'custom-fields',
 				'page-attributes',
 			),
-				'menu_icon' 			=> 'dashicons-book-alt',
-				'taxonomies' 			=> array( 'post_tag' )
+				'menu_icon' 					=> 'dashicons-book-alt',
+				'taxonomies' 					=> array( 'post_tag' )
 		));	
 	}
 public function prepend_event_data( $content ) {
  if( is_singular( 'eventing' ) ) {
- 		$eventDate = get_post_meta( get_the_ID(), 'Event Date', true );
- 		$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
- 		$url = get_post_meta( get_the_ID(), 'Link to the book:' , true);
- 		$html = '<div class = "event-meta">
-			<strong>Event date: </strong> '.$eventDate.'<br>
-			<strong>Event Location: </strong> '.$eventLocation.'<br>
-			</div>';
- 	include 'buttons.html';
+ 		include 'extra.php';
+ 		include 'buttons.html';
  	return $content . $html;
 }
 return $content;
 }
 }
-new EventListings();
+
 
 
