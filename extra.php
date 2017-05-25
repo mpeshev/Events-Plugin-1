0b0ad025d6f6c95ba8383e9f84851cab07aa32b2
Bojidar Sokolov <bebosvg@abv.bg>
2017-05-25 16:32:43 +0300

    Update extra.php

--- extra.php
+++ extra.php
@@ -3,6 +3,6 @@
 	$eventLocation = get_post_meta( get_the_ID(), 'Event Location' , true);
 	$url = get_post_meta( get_the_ID(), 'Link to the book:' , true);
 $html = '<div class = "event-meta">
-			<strong>Event date: </strong> '.$eventDate.'<br>
-			<strong>Event Location: </strong> '.$eventLocation.'<br>
-			</div>';
+	<strong>Event date: </strong> '.$eventDate.'<br>
+	<strong>Event Location: </strong> '.$eventLocation.'<br>
+	</div>';
